1e63f0dddde14f46073a7747fef5d1a467721f90
Matthew Burd <Burdman23@yahoo.com>
2018-10-18 00:17:36 -0400

    lab part 2

--- conditions.js
+++ conditions.js
@@ -1,38 +1,43 @@
 "use strict";
+let playerHealth = 40;
+let grantHealth = 10;
+let playerWins = 0;
 
-let play = confirm("Do you want to play?")
-if (play === "yes") {
-    //prompt to play the game
-    let playerName = prompt("Enter your name.");
-    let playerHealth = 40;
-    let grantHealth = 10;
-    let playerWins = 0;
+const startGame = () => {
+    const play = prompt("Do you want to play?")
+    if (play === "yes") {
+        //prompt to play the game
+        let playerName = prompt("Enter your name.");
+
+        startCombat(play, playerName)
+    };
+}
+function getDamage() {
+    return Math.floor(Math.random() * 4) + 1;
+}
+function startCombat(play, playerName) {
     while (play) {
-        userHealth -= Math.floor(Math.random() * 2) + 1;
-        grantHealth -= Math.floor(Math.random() * 2) + 1;
-        console.log(`${userName} was attacked. ${userName} has ${userHealth} health left.`);
+        const menu = prompt("Would you like to attack or quit?")
+        if (menu = "attack") 
+        playerHealth -= getDamage()
+        grantHealth -= getDamage()
         console.log(`Grant was attacked. Grant has ${grantHealth} health left.`);
+        console.log(`${playerName} was attacked. Player has ${playerHealth} health left.`);
         if (grantHealth <= 0) {
-            userWins++;
+            playerWins++;
             grantHealth = 10;
-            if (userWins === 3) {
-                console.log(`${userName} has won`);
+            if (playerWins === 3) {
+                console.log(`${playerName} has won`);
                 break;
             } else {
-                console.log(`${userName} has beaten Grant. ${userName} must win ${3 - userWins} more time(s).`);
+                console.log(`${playerName} has beaten Grant. ${playerName} must win ${3 - playerWins} more time(s).`);
             }
         }
-        if (userHealth <= 0) {
+        else if (playerHealth <= 0) {
             console.log("Grant has won!");
-            break;
+            return;
         }
     }
-    //if (yes) {
-    // window.prompt("please enter your name")
-    // makes the window pop out to ask my name
-    //console.log("name")
-    // Isn't correct need to figure out how to get console log to change
-    // to whatever name is entered. Green'd this out was unnecessary.
-} else {
-    console.log("Try again later.");
 }
+
+startGame()
